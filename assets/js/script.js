cd757716aec2eea1821a8a72f9411628126bcc28
JFalk21 <julianfalk2112@gmail.com>
2023-01-22 16:47:00 -0300

    Alterações textuais

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
 	        t = e.getMonth(),
 	        o = e.getFullYear();
 	        t = t + 1;
-	    document.getElementById("date-label").innerHTML = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"][e.getDay()] + ", " + a + "/" + t + "/" + o, $(".js-tilt").tilt({
+	    document.getElementById("date-label").innerHTML = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"][e.getDay()] + " dia " + a + "/" + t + "/" + o, $(".js-tilt").tilt({
 	        glare: !0,
 	        maxGlare: .5,
 	        scale: 1.2
